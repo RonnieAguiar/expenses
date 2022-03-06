b028efd537dac0df2b8b787f7dce9a6811306141
Ronnie Aguiar <ronnieaguiar@yahoo.com.br>
2022-03-06 19:43:40 -0300

    Corrigindo tamanho do layout em orientação paisagem

--- lib/components/transactions_list.dart
+++ lib/components/transactions_list.dart
@@ -6,35 +6,39 @@ class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
   final void Function(String) onRemove;
 
-  const TransactionList(this.transactions, this.onRemove, {Key? key}) : super(key: key);
+  const TransactionList(this.transactions, this.onRemove, {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? Column(children: [
-            const SizedBox(height: 20),
-            const Text('Nenhuma transação cadastrada'),
-            const SizedBox(height: 20),
-            SizedBox(
-              height: 200,
-              child: Image.asset(
-                'assets/images/waiting.png',
-                fit: BoxFit.cover,
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(children: [
+              SizedBox(height: constraints.maxHeight * 0.05),
+              SizedBox(
+                height: constraints.maxHeight * 0.3,
+                child: const Text('Nenhuma transação cadastrada'),
               ),
-            ),
-          ])
+              SizedBox(height: constraints.maxHeight * 0.05),
+              SizedBox(
+                height: constraints.maxHeight * 0.6,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ]);
+          })
         : ListView.builder(
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final tr = transactions[index];
               return Card(
                 elevation: 5,
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+                margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 8),
                 child: ListTile(
                   leading: CircleAvatar(
-                      backgroundColor:
-                          Theme.of(context).colorScheme.primary,
+                      backgroundColor: Theme.of(context).colorScheme.primary,
                       radius: 30,
                       child: Padding(
                         padding: const EdgeInsets.all(6.0),
